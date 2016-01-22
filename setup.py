2c1abe8820c21cb9cda5ed58ae44872e01deff56
Clay McLeod <clay.l.mcleod@gmail.com>
2016-01-22 15:53:40 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='OpenDNN',
+      version='1.0.0',
+      description='Open deep neural network framework',
+      author='Clay McLeod',
+      author_email='clay.l.mcleod@gmail.com',
+      url='https://github.com/claymcleod/opendnn',
+      license='MIT',
+      install_requires=['theano'],
+      packages=find_packages()
+)
