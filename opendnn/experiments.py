ffc1a52cbcda8e2ad0dfdbf961c0aab0e33ba32b
Clay McLeod <clay.l.mcleod@gmail.com>
2016-01-26 15:46:56 -0600

    added activationpool

--- opendnn/experiments.py
+++ opendnn/experiments.py
@@ -1,3 +1,4 @@
+import numpy
 import theano
 from opendnn.layers import Activation, Layer
 
@@ -29,3 +30,31 @@ class MReLU(Layer):
             return updates
         else:
             return []
+
+class ActivationPool(Layer):
+
+    def __init__(self, activation_fns, weights_init=None, *args, **kwargs):
+        assert(len(activation_fns) > 0), ("Must have at least one activation function!")
+        if weights_init is None:
+            weights_init = [1. / len(activation_fns)] * len(activation_fns)
+
+        assert(len(activation_fns) == len(weights_init)), ("Must have the same number of "
+                                                           "weights as activation_fns.")
+
+        self.activation_fns = activation_fns
+        self.weights_init = weights_init
+        super(ActivationPool, self).__init__(*args, **kwargs)
+
+    def _build_layer_(self, X, layer_num, input_dim, output_dim, init_fn, use_normal):
+        self.activation_weights = []
+        for x, activation_fn in enumerate(self.activation_fns):
+            this_weight = theano.shared(self.weights_init[x], name='AP_w{}'.format(x))
+            X = this_weight * activation_fn(X)
+            self.activation_weights.append(this_weight)
+        return X
+
+    def _get_updates_(self, loss, learning_rate):
+        updates = []
+        for a in self.activation_weights:
+            updates.append((a, a - theano.tensor.grad(loss, a) * learning_rate))
+        return updates
